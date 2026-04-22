7a16f4c3cce4f89a4ec9dfc659c440eccd7edbaa
André Carvalho <afacarvalho@yahoo.com.br>
2026-04-22 16:35:50 -0300

    Change build

--- docker-build.sh
+++ docker-build.sh
@@ -4,13 +4,13 @@
 #docker container stop SICAR-Appeals-Analysis-API
 
 VERSION=$(git describe --tags --abbrev=0)
-export VERSION
+
 # build all images
-docker build --no-cache -t terrabrasilis/sicar-appeals-analysis-api:$VERSION --build-arg VERSION=$VERSION -f ./Dockerfile .
+docker build --no-cache -t terrabrasilis/sicar-appeals-analysis-api:v$VERSION -f ./Dockerfile .
 
 # send to dockerhub
 docker login
-docker push terrabrasilis/sicar-appeals-analysis-api:$VERSION
+docker push terrabrasilis/sicar-appeals-analysis-api:v$VERSION
 
 # If you want run containers, uncomment this lines
 #docker run -d --rm -p 84:80 --name SICAR-Appeals-Analysis-API terrabrasilis/sicar-appeals-Analysis-API
